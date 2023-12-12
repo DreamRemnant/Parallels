9092b1174a988e3960a18cb1c27f7b41e0bfd55c
DreamRemnant <jfmg.64@gmail.com>
2023-12-12 00:45:13 -0700

    Se crearon dos métodos para contar vocales: ContarVocales y ContarVocalesParalelo. Se añadió una llamada en main mostrando el tiempo que tarda cada uno en milisegundos.

--- VowelCount/Program.cs
+++ VowelCount/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
 
 namespace FileTransfer
 {
@@ -6,20 +7,29 @@ namespace FileTransfer
     {
         static void Main(string[] args)
         {
-            contarVocales();
+            List<char> vowels = new List<char> { 'a', 'e', 'i', 'o', 'u' };
+            var path = Path.Combine(Environment.CurrentDirectory, "../../../Lorem Ipsum.txt");
+            Double tiempoSincrono = 0.0;
+            Double tiempoParalelo = 0.0;
+
+            Console.WriteLine("Contando vocales con método síncrono.");
+            tiempoSincrono = ContarVocales(vowels, path);
+            Console.WriteLine("Contando vocales con método paralelo.");
+            tiempoParalelo = ContarVocalesParalelo(vowels, path);
+
+            Console.WriteLine($"El método síncrono tardó {tiempoSincrono - tiempoParalelo} más que el paralelo.");
+            Console.ReadLine();
         }
 
-        private static void contarVocales()
+        private static double ContarVocales(List<char> vows, string Lorem)
         {
             Stopwatch sw = new Stopwatch();
             sw.Start();
-            List<char> vowels = new List<char> { 'a', 'e', 'i', 'o', 'u' };
-            var path = Path.Combine(Environment.CurrentDirectory, "../../../Lorem Ipsum.txt");
-            string content = File.ReadAllText(path);
+            string content = File.ReadAllText(Lorem);
             int vowelCount = 0;
             foreach (char c in content)
             {
-                if (vowels.Contains(c))
+                if (vows.Contains(c))
                 {
                     vowelCount++;
                 }
@@ -28,7 +38,31 @@ namespace FileTransfer
             TimeSpan ts = sw.Elapsed;
             string elapsedTime = ts.TotalMilliseconds.ToString();
             Console.WriteLine($"Se encontraron {vowelCount} vocales en el texto.");
-            Console.WriteLine($"Tiempo de ejecucion: {elapsedTime} milisegundos");
+            Console.WriteLine($"Tiempo de ejecucion: {elapsedTime} milisegundos\n");
+            return ts.TotalMilliseconds;
+        }
+
+        private static double ContarVocalesParalelo(List<char> vows, string Lorem)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            sw.Start();
+            string content = File.ReadAllText(Lorem);
+            ConcurrentBag<char> foundVowels = new ConcurrentBag<char>();
+            
+            Parallel.ForEach(content, character => {
+                if (vows.Contains(character))
+                {
+                    foundVowels.Add(character);
+                }
+            });
+            sw.Stop();
+            int vowelCount = foundVowels.Count();
+
+            TimeSpan ts = sw.Elapsed;
+            string elapsedTime = ts.TotalMilliseconds.ToString();
+            Console.WriteLine($"Se encontraron {vowelCount} vocales en el texto.");
+            Console.WriteLine($"Tiempo de ejecucion: {elapsedTime} milisegundos\n");
+            return ts.TotalMilliseconds;
         }
     }
 }
