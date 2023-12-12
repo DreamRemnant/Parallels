afdee8f3a43e245eb6110d238b3c91e29dc5e355
DreamRemnant <jfmg.64@gmail.com>
2023-12-11 23:30:11 -0700

    Se crearon los archivos Program, el método ContarVocales y se añadió "Lorem Ipsum.txt" ContarVocales cuenta las vocales dentro del texto.

---
+++ FileTransfer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+
+namespace FileTransfer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            contarVocales();
+        }
+
+        private static void contarVocales()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            List<char> vowels = new List<char> { 'a', 'e', 'i', 'o', 'u' };
+            var path = Path.Combine(Environment.CurrentDirectory, "../../../Lorem Ipsum.txt");
+            string content = File.ReadAllText(path);
+            int vowelCount = 0;
+            foreach (char c in content)
+            {
+                if (vowels.Contains(c))
+                {
+                    vowelCount++;
+                }
+            }
+            sw.Stop();
+            TimeSpan ts = sw.Elapsed;
+            string elapsedTime = ts.TotalMilliseconds.ToString();
+            Console.WriteLine($"Se encontraron {vowelCount} vocales en el texto.");
+            Console.WriteLine($"Tiempo de ejecucion: {elapsedTime} milisegundos");
+        }
+    }
+}
